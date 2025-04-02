065421ce35d048958716b72917ec2d80e13a7a3d
Nikhil Singh <nikhilpramodsingh@gmail.com>
2025-04-02 18:11:55 +0530

    Fixed unused imports issue

--- src/app/HomePage/spotlight/hero.tsx
+++ src/app/HomePage/spotlight/hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { useState } from "react"
+
 import { motion } from "framer-motion"
 import {  clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
